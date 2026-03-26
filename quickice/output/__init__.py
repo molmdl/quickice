69fa82951045db772c4defd3ab750617eda7f29a
molmdl <133654677+molmdl@users.noreply.github.com>
2026-03-27 02:36:44 +0800

    feat(05-04): export generate_phase_diagram from output module

--- quickice/output/__init__.py
+++ quickice/output/__init__.py
@@ -3,6 +3,7 @@
 from quickice.output.types import OutputResult
 from quickice.output.pdb_writer import write_pdb_with_cryst1, write_ranked_candidates
 from quickice.output.validator import validate_space_group, check_atomic_overlap
+from quickice.output.phase_diagram import generate_phase_diagram
 
 __all__ = [
     "OutputResult",
@@ -10,4 +11,5 @@ __all__ = [
     "write_ranked_candidates",
     "validate_space_group",
     "check_atomic_overlap",
+    "generate_phase_diagram",
 ]
