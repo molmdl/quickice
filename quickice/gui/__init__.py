9a175468701a6ed760a68c98eac763ebf436201a
molmdl <133654677+molmdl@users.noreply.github.com>
2026-04-15 04:50:41 +0800

    feat(30-04): export ion_renderer from quickice.gui module
    - Add NA_COLOR, CL_COLOR, NA_VDW_RADIUS, CL_VDW_RADIUS exports
    - Add create_na_actor, create_cl_actor, render_ion_structure exports
    - Add add_ion_actors_to_viewer, remove_ion_actors_from_viewer, toggle_ion_visibility
    - Update __all__ with all ion_renderer functions

--- quickice/gui/__init__.py
+++ quickice/gui/__init__.py
@@ -4,5 +4,34 @@ from quickice.gui.main_window import MainWindow, run_app
 from quickice.gui.phase_diagram_widget import PhaseDiagramPanel
 from quickice.gui.hydrate_panel import HydratePanel
 from quickice.gui.ion_panel import IonPanel
+from quickice.gui.ion_renderer import (
+    NA_COLOR,
+    CL_COLOR,
+    NA_VDW_RADIUS,
+    CL_VDW_RADIUS,
+    create_na_actor,
+    create_cl_actor,
+    render_ion_structure,
+    add_ion_actors_to_viewer,
+    remove_ion_actors_from_viewer,
+    toggle_ion_visibility,
+)
 
-__all__ = ["MainWindow", "run_app", "PhaseDiagramPanel", "HydratePanel", "IonPanel"]
+__all__ = [
+    "MainWindow",
+    "run_app",
+    "PhaseDiagramPanel",
+    "HydratePanel",
+    "IonPanel",
+    # Ion renderer
+    "NA_COLOR",
+    "CL_COLOR",
+    "NA_VDW_RADIUS",
+    "CL_VDW_RADIUS",
+    "create_na_actor",
+    "create_cl_actor",
+    "render_ion_structure",
+    "add_ion_actors_to_viewer",
+    "remove_ion_actors_from_viewer",
+    "toggle_ion_visibility",
+]
