f90caf536069ff3edd9ef99d34ef1c0dc2dab841
molmdl <133654677+molmdl@users.noreply.github.com>
2026-04-15 04:41:59 +0800

    feat(30-03): export IonPanel from quickice.gui
    - Added IonPanel to __all__ list
    - Added import from ion_panel module

--- quickice/gui/__init__.py
+++ quickice/gui/__init__.py
@@ -3,5 +3,6 @@
 from quickice.gui.main_window import MainWindow, run_app
 from quickice.gui.phase_diagram_widget import PhaseDiagramPanel
 from quickice.gui.hydrate_panel import HydratePanel
+from quickice.gui.ion_panel import IonPanel
 
-__all__ = ["MainWindow", "run_app", "PhaseDiagramPanel", "HydratePanel"]
+__all__ = ["MainWindow", "run_app", "PhaseDiagramPanel", "HydratePanel", "IonPanel"]
